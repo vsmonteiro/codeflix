482d811065e16cb80fbce5afe5eb61334ee9236a
Vinicius Monteiro <monteiro_vinicius@outlook.com>
2023-10-17 21:48:44 -0300

    add transaction model

---
+++ domain/model/transaction.go
@@ -0,0 +1,107 @@
+package model
+
+import (
+	"errors"
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	"github.com/google/uuid"
+)
+
+const (
+	TransactionPending   string = "pending"
+	TransactionCompleted string = "completed"
+	TransactionError     string = "error"
+	TransactionConfirmed string = "confirmed"
+)
+
+type Transactions struct {
+	Transaction []Transaction
+}
+
+type TransactionRepositoryInterface interface {
+	Register(transaction *Transaction) error
+	Save(transaction *Transaction) error
+	Find(id string) (*Transaction, error)
+}
+
+type Transaction struct {
+	Base              `valid:"required"`
+	AccountFrom       *Account `valid:"-"`
+	Amount            float64  `json:"amount" valid:"notnull"`
+	PixKeyTo          *PixKey  `valid:"-"`
+	Status            string   `json:"status" valid:"notnull"`
+	Description       string   `json:"description" valid:"notnull"`
+	CancelDescription string   `json:"cancel_description" valid:"-"`
+}
+
+func (transaction *Transaction) isValid() error {
+	_, err := govalidator.ValidateStruct(transaction)
+
+	if transaction.Amount <= 0 {
+		return errors.New("the amount must be greater than 0")
+	}
+
+	if transaction.Status != TransactionCompleted && transaction.Status != TransactionConfirmed && transaction.Status != TransactionError && transaction.Status != TransactionPending {
+		return errors.New("invalid status")
+	}
+
+	if transaction.PixKeyTo.AccountID == transaction.AccountFrom.ID {
+		return errors.New("the source and origin cannot be the same")
+	}
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewTransaction(accountFrom *Account, amount float64, pixKeyTo *PixKey, description string) (*Transaction, error) {
+	transaction := Transaction{
+		AccountFrom: accountFrom,
+		Amount:      amount,
+		PixKeyTo:    pixKeyTo,
+		Description: description,
+		Status:      TransactionPending,
+	}
+
+	transaction.ID = uuid.NewString()
+	transaction.CreatedAt = time.Now()
+
+	err := transaction.isValid()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &transaction, nil
+}
+
+func (transaction *Transaction) Complete() error {
+	transaction.Status = TransactionCompleted
+	transaction.UpdatedAt = time.Now()
+
+	err := transaction.isValid()
+
+	return err
+}
+
+func (transaction *Transaction) Confirm() error {
+	transaction.Status = TransactionConfirmed
+	transaction.UpdatedAt = time.Now()
+
+	err := transaction.isValid()
+
+	return err
+}
+
+func (transaction *Transaction) Cancel(description string) error {
+	transaction.Status = TransactionError
+	transaction.UpdatedAt = time.Now()
+	transaction.CancelDescription = description
+
+	err := transaction.isValid()
+
+	return err
+}
